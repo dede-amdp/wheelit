77514b090c79960736bea1c860731887323828ed
dede-amdp <73134218+dede-amdp@users.noreply.github.com>
2020-12-08 16:34:16 +0100

    Inizio del lavoro su gmaps

--- lib/activity/HomeScreen.dart
+++ lib/activity/HomeScreen.dart
@@ -5,6 +5,7 @@ import 'package:wheelit/classes/DatabaseManager.dart';
 import 'package:wheelit/activity/DrawerScreen.dart';
 import 'package:wheelit/classes/BottomBar.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -15,58 +16,80 @@ class _HomeScreenState extends State<HomeScreen> {
   Map mezzi;
   Ticket recent;
   String userEmail = 'pippolippo@gmail.com';
+  LatLng userLocation;
 
   @override
   void initState() {
-    getData(userEmail);
+    getData();
+    getLocation();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        drawer: Drawer(child: DrawerScreen()),
-        body: Builder(
-          builder: (context) {
-            return Stack(
-              children: [
-                GoogleMap(
-                  initialCameraPosition: CameraPosition(
-                      zoom: 15, target: LatLng(41.092724, 16.852686)),
-                ),
-                SafeArea(
-                  child: Align(
-                      alignment: Alignment.topLeft,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          IconButton(
-                              iconSize: 32.0,
-                              icon: Icon(
-                                Icons.menu,
-                                color: Theme.of(context).accentColor,
-                              ),
-                              onPressed: () {
-                                Scaffold.of(context).openDrawer();
-                              }),
-                          SearchBar(),
-                        ],
-                      )),
-                ),
-                SafeArea(
-                    child: Align(
-                        alignment: Alignment.bottomCenter, child: BottomBar()))
-              ],
-            );
-          },
-        ));
+    return mezzi == null || userLocation == null
+        ? Scaffold(body: Center(child: CircularProgressIndicator()))
+        : Scaffold(
+            drawer: Drawer(child: DrawerScreen()),
+            body: Builder(
+              builder: (context) {
+                return Stack(
+                  children: [
+                    GoogleMap(
+                      myLocationEnabled: true,
+                      markers: Set.from(mezzi.entries.map((e) {
+                        return e.value['state'] != "FREE"
+                            ? null
+                            : Marker(
+                                markerId: MarkerId(e.key),
+                                position: LatLng(e.value['position'].latitude,
+                                    e.value['position'].longitude));
+                      })),
+                      initialCameraPosition:
+                          CameraPosition(zoom: 15, target: userLocation),
+                    ),
+                    SafeArea(
+                      child: Align(
+                          alignment: Alignment.topLeft,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconButton(
+                                  iconSize: 32.0,
+                                  icon: Icon(
+                                    Icons.menu,
+                                    color: Theme.of(context).accentColor,
+                                  ),
+                                  onPressed: () {
+                                    Scaffold.of(context).openDrawer();
+                                  }),
+                              SearchBar(),
+                            ],
+                          )),
+                    ),
+                    SafeArea(
+                        child: Align(
+                            alignment: Alignment.bottomCenter,
+                            child: BottomBar()))
+                  ],
+                );
+              },
+            ));
   }
 
-  Future<void> getData(String userEmail) async {
-    Ticket lastTicket = (await DatabaseManager.getTicketData(userEmail))['0'];
+  Future<void> getData() async {
+    Map mezziMap = await DatabaseManager.getTransportData(public: false);
     setState(() {
-      this.recent = lastTicket;
-      this.mezzi = {};
+      this.mezzi = mezziMap;
+    });
+  }
+
+  Future<void> getLocation() async {
+    Location l = Location();
+    l.onLocationChanged().listen((loc) {
+      setState(() {
+        userLocation = LatLng(loc.latitude, loc.longitude);
+      });
     });
   }
 }
